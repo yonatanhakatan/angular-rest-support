5fac2b3122ae5f69f2c3c59be852eef5406ae4d7
Jonny Levy <iamjonnylevy@gmail.com>
2015-12-07 17:41:28 +0000

    refactor(test): consolidate all invalid end-point suites into one suite

--- src/tests/unit/ars.spec.js
+++ src/tests/unit/ars.spec.js
@@ -2,6 +2,7 @@ describe('Angular Rest Support', function() {
   var $httpBackend;
   var arsHelper;
   var dataBuilder = jsonApiDataBuilder();
+  var httpMethods = ['delete', 'get', 'patch', 'post', 'put'];
 
   beforeEach(function() {
     module('ars');
@@ -58,26 +59,6 @@ describe('Angular Rest Support', function() {
     });
   });
 
-  describe('When calling an invalid DELETE end-point', function() {
-    var deleteRequest;
-
-    beforeEach(function() {
-      deleteRequest = arsHelper
-        .delete('/badurl')
-        .request();
-    });
-
-    it('Should return the correct http status code', function() {
-      var httpStatus;
-      deleteRequest
-        .then(function(success) {}, function(fail) {
-          httpStatus = fail.status;
-        });
-      $httpBackend.flush();
-      expect(httpStatus).toEqual(400);
-    });
-  });
-
   describe('When calling a valid GET end-point', function() {
     var getRequest;
 
@@ -100,26 +81,6 @@ describe('Angular Rest Support', function() {
         });
       $httpBackend.flush();
       expect(returnedData).toEqual(dataBuilder.allAuthors);
-    });
-  });
-
-  describe('When calling an invalid GET end-point', function() {
-    var getRequest;
-
-    beforeEach(function() {
-      getRequest = arsHelper
-        .get('/badurl')
-        .request();
-    });
-
-    it('Should return the correct http status code', function() {
-      var httpStatus;
-      getRequest
-        .then(function(success) {}, function(fail) {
-          httpStatus = fail.status;
-        });
-      $httpBackend.flush();
-      expect(httpStatus).toEqual(400);
     });
   });
 
@@ -149,26 +110,6 @@ describe('Angular Rest Support', function() {
     });
   });
 
-  describe('When calling an invalid PATCH end-point', function() {
-    var patchRequest;
-
-    beforeEach(function() {
-      patchRequest = arsHelper
-        .patch('/badurl')
-        .request();
-    });
-
-    it('Should return the correct http status code', function() {
-      var httpStatus;
-      patchRequest
-        .then(function(success) {}, function(fail) {
-          httpStatus = fail.status;
-        });
-      $httpBackend.flush();
-      expect(httpStatus).toEqual(400);
-    });
-  });
-
   describe('When calling a valid POST end-point', function() {
     var postData = {name: 'John Doe', dob: '1982-01-01'};
     var postRequest;
@@ -192,26 +133,6 @@ describe('Angular Rest Support', function() {
         });
       $httpBackend.flush();
       expect(returnedData).toEqual(postData);
-    });
-  });
-
-  describe('When calling an invalid POST end-point', function() {
-    var postRequest;
-
-    beforeEach(function() {
-      postRequest = arsHelper
-        .post('/badurl')
-        .request();
-    });
-
-    it('Should return the correct http status code', function() {
-      var httpStatus;
-      postRequest
-        .then(function(success) {}, function(fail) {
-          httpStatus = fail.status;
-        });
-      $httpBackend.flush();
-      expect(httpStatus).toEqual(400);
     });
   });
 
@@ -290,23 +211,25 @@ describe('Angular Rest Support', function() {
     });
   });
 
-  describe('When calling an invalid PUT end-point', function() {
-    var putRequest;
+  describe('When calling an invalid end-point', function() {
+    for (var key in httpMethods) {
+      var httpMethod = httpMethods[key];
+      var request;
 
-    beforeEach(function() {
-      putRequest = arsHelper
-        .put('/badurl')
-        .request();
-    });
+      beforeEach(function() {
+        request = arsHelper[httpMethod]('/badurl').request();
+      });
 
-    it('Should return the correct http status code', function() {
-      var httpStatus;
-      putRequest
-        .then(function(success) {}, function(fail) {
-          httpStatus = fail.status;
-        });
-      $httpBackend.flush();
-      expect(httpStatus).toEqual(400);
-    });
+      it('Should return the correct http status code', function() {
+        var httpStatus;
+        request
+          .then(function(success) {}, function(fail) {
+            httpStatus = fail.status;
+          });
+        $httpBackend.flush();
+        expect(httpStatus).toEqual(400);
+      });
+    }
   });
+
 });
