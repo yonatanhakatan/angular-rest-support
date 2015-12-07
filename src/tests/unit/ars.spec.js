69f195bc5a52e004bb7839f09a5e1973f487c656
Jonny Levy <iamjonnylevy@gmail.com>
2015-12-07 15:47:17 +0000

    test(POST): test valid POST Request

--- src/tests/unit/ars.spec.js
+++ src/tests/unit/ars.spec.js
@@ -13,6 +13,10 @@ describe('Angular Rest Support', function() {
     // Mock the Data
     $httpBackend.whenGET('/authors').respond(200, dataBuilder.allAuthors);
     $httpBackend.whenGET('/badurl').respond(400);
+
+    $httpBackend.whenPOST('/authors').respond(function(method, url, data, headers) {
+      return [200, data];
+    });
   });
 
   describe('When calling a valid GET end-point', function() {
@@ -57,6 +61,32 @@ describe('Angular Rest Support', function() {
         });
       $httpBackend.flush();
       expect(httpStatus).toEqual(400);
+    });
+  });
+
+  describe('When calling a valid POST end-point', function() {
+    var postData = {name: 'John Doe', dob: '1982-01-01'};
+    var postRequest;
+
+    beforeEach(function() {
+      postRequest = arsHelper
+        .post('/authors', postData)
+        .request();
+    });
+
+    it('Should make a POST request', function() {
+      $httpBackend.expectPOST('/authors');
+      $httpBackend.flush();
+    });
+
+    it('Should return the correct data', function() {
+      var returnedData;
+      postRequest
+        .then(function(success) {
+          returnedData = success.data;
+        });
+      $httpBackend.flush();
+      expect(returnedData).toEqual(postData);
     });
   });
 });
